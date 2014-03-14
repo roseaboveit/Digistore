6190284aac8eb9ebce57007161c379d4652edaa7
Whitney Levis <ms.whitney.levis@gmail.com>
2014-03-14 10:06:19 -0700

    Added price to the homepage

--- app/scripts/templates.js
+++ app/scripts/templates.js
@@ -112,6 +112,9 @@ function program2(depth0,data) {
   data.buffer.push(" class='thumbnail col-lg-12' />\n          <br>\n          ");
   stack1 = helpers._triageMustache.call(depth0, "name", {hash:{},hashTypes:{},hashContexts:{},contexts:[depth0],types:["ID"],data:data});
   if(stack1 || stack1 === 0) { data.buffer.push(stack1); }
+  data.buffer.push("\n          <br>\n          ");
+  stack1 = helpers._triageMustache.call(depth0, "dollars", {hash:{},hashTypes:{},hashContexts:{},contexts:[depth0],types:["ID"],data:data});
+  if(stack1 || stack1 === 0) { data.buffer.push(stack1); }
   data.buffer.push("\n          </div>\n        ");
   return buffer;
   }
