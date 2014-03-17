d1c1165da3a3c07a11a70a5d4f7b678bdd5c433e
Whitney Levis <ms.whitney.levis@gmail.com>
2014-03-17 15:26:59 -0700

    removed unneeded fixture data from item model

--- app/scripts/models/item.js
+++ app/scripts/models/item.js
@@ -10,13 +10,3 @@ App.Item = DS.Model.extend({
           return "$" + this.get("subtotal")/100;
      }.property("price", "quantity")
 })
-
-App.Item.FIXTURES = [
-  {id: 1,
-  quantity: 1,
-  cart: 1,
-  product: 1,
-  price: 749,
-  subtotal: 749
-  }  
-]
