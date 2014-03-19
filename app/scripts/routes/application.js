4cc47e8f12fc99a7e344ff6804193a252a29dcb2
Whitney Levis <ms.whitney.levis@gmail.com>
2014-03-19 13:30:03 -0700

    correctly interfaces with rails app to create a new cart and use it

--- app/scripts/routes/application.js
+++ app/scripts/routes/application.js
@@ -1,6 +1,10 @@
 App.ApplicationRoute = Ember.Route.extend({
 
   beforeModel: function () {
+    
+  },
+
+  model: function(){
     if (typeof(localStorage.cartId) === "undefined"){
       var cart = this.store.createRecord("cart")
       cart.save().then(
@@ -15,9 +19,7 @@ App.ApplicationRoute = Ember.Route.extend({
         );
       
     }
-  },
 
-  model: function(){
     return this.store.find("cart", localStorage.cartId)
   }
 })
